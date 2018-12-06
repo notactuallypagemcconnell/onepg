76d990defc1b316ae598d5e9e77e191b4b750876
Bobby 'Bobdawg' Grayson <bobbbygrayson@gmail.com>
2018-12-06 14:33:24 -0500

    refactor to be like an actual class lol

--- main.rb
+++ main.rb
@@ -2,13 +2,40 @@
 
 require 'kramdown'
 
-def main
-  base = File.read("index.html")
+class OnePageSite
+  def initialize(posts_directory = "/posts")
+    @posts_directory = posts_directory
+  end
 
-  html_docs = `ls posts`.split("\n").map do |file|
-                Kramdown::Document.new(File.read("posts/#{file}")).to_html
-              end
-  puts base + "</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>" + html_docs.join("</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>")
+  def generate
+    puts full_page
+  end
+
+  def full_page
+    "#{base}#{page_break}#{html_posts.join(page_break)}"
+  end
+
+  def html_posts
+    posts.map { |post| post_to_html(post) }
+  end
+
+  def post_to_html(post)
+    Kramdown::Document.new(File.read("posts/#{post}")).to_html
+  end
+
+  def posts
+    `ls posts`.split("\n")
+  end
+
+  def base
+    File.read("index.html")
+  end
+
+  def page_break
+    "</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>"
+  end
 end
 
-main
+html = OnePageSite.new.generate
+
+puts html
---
+++ posts/two.md
@@ -0,0 +1,1 @@
+# this is another would-be post
