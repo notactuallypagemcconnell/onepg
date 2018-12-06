b2f1968a880da7bc716a0a068cb501dbf7858b77
Bobby 'Bobdawg' Grayson <bobbbygrayson@gmail.com>
2018-12-06 18:13:11 -0500

    start automating my dope banners

--- main.rb
+++ main.rb
@@ -39,7 +39,35 @@ class OnePageSite
   end
 end
 
-html = OnePageSite.new.generate
+class BasePageBuilder
+  def initialize(phrase, header_size = 2)
+    words = phrase.split(" ")
+    tags = words.map do |word|
+      colors = ["red", "blue", "green"]
+      chars = word.split("")
+      result =
+        chars.map.with_index do |char, i|
+          if i == chars.length - 1
+            # add a space if its the end of the word for the next one
+            "<span style='color: #{colors.sample}'>#{char}</br></span>\n"
+          else
+            "<span style='color: #{colors.sample}'>#{char}</span>\n"
+          end
+        end.join
+      a = "<h#{header_size}>#{result}</h#{header_size}>"
+      binding.pry
+      a
+    end
+  end
 
+  def to_colored_banner
+  end
+end
+# a = BasePageBuilder.new("I'm Bobby")
+# b = BasePageBuilder.new("I'm a programmer in New York")
+# binding.pry
+html = OnePageSite.new.generate
+ 
 puts html
 File.write("out.html", html)
+
