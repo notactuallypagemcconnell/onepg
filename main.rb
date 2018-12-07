29771af64bdcf888f88d2ce369ec4828ffb96c9a
Bobby 'Bobdawg' Grayson <bobbbygrayson@gmail.com>
2018-12-06 19:28:01 -0500

    clean up the about me generator

--- main.rb
+++ main.rb
@@ -42,27 +42,34 @@ end
 
 class AboutMePage
   # this however, not so much.
-  attr_reader :html
+  attr_reader :html, :colors, :header_size
 
   def initialize(phrase, header_size = 2)
+    # i know light blue isnt a color to the browser in this string form, I just like it there
+    @colors = ["red", "light blue", "orange"]
+    @header_size = header_size
     words = phrase.split(" ")
-    tags = words.map do |word|
-      colors = ["red", "light blue", "orange"] # i know light blue isnt a color to the browser in this string form, I just like it there
-      chars = word.split("")
-      result =
-        chars.map.with_index do |char, i|
-          if i == chars.length - 1
-            # add a space if its the end of the word for the next one
-            "<span style='color: #{colors.sample}'>#{char}</br></span>\n"
-          else
-            "<span style='color: #{colors.sample}'>#{char}</span>\n"
-          end
-        end.join
-      a = "<h#{header_size}>#{result}</h#{header_size}>"
-      a
+    headered_span_words = create_headered_span_words(words)
+    about_me_page_that_needs_closing_html_and_body_tags = base_doc + buttons + "<div>" + headered_span_words + "</div>"
+    # simple api, documenting by name. so clever. lol
+    @html = about_me_page_that_needs_closing_html_and_body_tags
+  end
+
+  def create_headered_span_words(words)
+    words.map do |word|
+      chars         = word.split("")
+      spans         = chars.map.with_index { |char, i| span_for_character(char, chars, i) }.join
+      headered_span = "<h#{header_size}>#{spans}</h#{header_size}>"
+    end.join
+  end
+
+  def span_for_character(char, chars, index)
+    if index == chars.length - 1
+      # add a break if its the end of the word for the next one
+      "<span style='color: #{colors.sample}'>#{char}</br></span>\n"
+    else
+      "<span style='color: #{colors.sample}'>#{char}</span>\n"
     end
-    a = base_doc + buttons + "<div>" + tags.join + "</div>"
-    @html = a
   end
 
   def base_doc
