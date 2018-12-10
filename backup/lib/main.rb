213966d13385e4265e611c91bf0280c02d5def13
Bobby 'Bobdawg' Grayson <bobbbygrayson@gmail.com>
2018-12-10 12:32:41 -0500

    lol wtf happened here

--- backup/lib/main.rb
+++
@@ -1,7 +0,0 @@
-#!/bin/ruby
-require 'pry'
-require 'kramdown'
-require_relative 'one_page_site'
-
-OnePageSite.new("Hi I'm Bobby. I'm a programmer in New York. My github username is @notactuallypagemcconnell.").generate
-puts "Site copied to clipboard"
